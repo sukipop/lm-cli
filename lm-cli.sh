833d4925a4c48fed7f190878246b647d105fccd9
sukipop <95154913+sukipop@users.noreply.github.com>
2022-01-20 05:32:37 -0700

    update lm-cli.sh

--- lm-cli.sh
+++ lm-cli.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if [ -n "$1" ] || [ "$1" = --help ] || [ "$1" = --h ] || [ "$1" = -h ]; then
+if [ "$1" = --help ] || [ "$1" = --h ] || [ -n "$1" ]; then
 	echo lords mobile - command line information
-	echo useage: lm-cli [--version] [--help]
-elif [ "$1" = --version ] || [ "$1" = --v ] || [ "$1" = -v ]; then
+	echo useage: lm-cli [--version] [--help | -h]
+elif [ "$1" = --version ] || [ "$1" = --v ]; then
 	echo lm-cli version 0.69.420
 elif [ "$1" = --hunt ] || [ "$1" = -hunt ] || [ "$1" = -hu ]; then
 	./hunt
