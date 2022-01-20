5242b68d96a1471ba5ea4858c577e51cca2565b0
sukipop <95154913+sukipop@users.noreply.github.com>
2022-01-20 05:24:04 -0700

    update lm-cli.sh

--- lm-cli.sh
+++ lm-cli.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-if [ $1 = --help ] || [ $1 = --h ] || [ $1 = -h ]; then
+if [ -n "$1" ] || [ "$1" = --help ] || [ "$1" = --h ] || [ "$1" = -h ]; then
 	echo lords mobile - command line information
 	echo useage: lm-cli [--version] [--help]
-elif [ $1 = --version ] || [ $1 = --v ] || [ $1 = -v ]; then
+elif [ "$1" = --version ] || [ "$1" = --v ] || [ "$1" = -v ]; then
 	echo lm-cli version 0.69.420
-elif [ $1 = --hunt ] || [ $1 = -hunt ] || [ $1 = -hu ]; then
+elif [ "$1" = --hunt ] || [ "$1" = -hunt ] || [ "$1" = -hu ]; then
 	./hunt
 else
-	echo lm-cli: $1 is not a lm-cli command. see 'lm-cli --help'
+	echo lm-cli: "$1" is not a lm-cli command. see 'lm-cli --help'
 fi
