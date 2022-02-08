2a0d6058b5400c4e4f529bbeb4f39675f88dcc58
sukipop <95154913+sukipop@users.noreply.github.com>
2022-02-08 17:01:53 +0000

    update lm-cli.sh

--- lm-cli.sh
+++ lm-cli.sh
@@ -1,53 +1,61 @@
 #!/bin/bash
 if [ "$1" = --hunt ] || [ "$1" = -h ]; then
 	if [ "$2" = -arctic] || [ "$2" = -flipper ] || [ "$2" = -maf ] || [ "$2" = -marc ] || [ "$2" = -mart ] || [ "$2" = -af ]; then
-		echo "arctic flipper"
+		monster="arctic flipper"
+		if [ "$3" = 1 ]; then
+			
+		elif [ "$3" = 2 ]; then
+			
+		elif [ "$3" = 3 ]; then
+			
+		elif [ "$3" = 4 ] || [ "$3" = 5 ]; then
+			
 	elif [ "$2" = -blackwing ] || [ "$2" = -mbw ] || [ "$2" = -mblk ] || [ "$2" = -blkw ] || [ "$2" = -bw ]; then
-		echo "blackwing"
+		monster="blackwing"
 	elif [ "$2" = -bon ] || [ "$2" = -appeti ] || [ "$2" = -mba ] || [ "$2" = -mbon ] || [ "$2" = -ba ]; then
-		echo "bon appeti"
+		monster="bon appeti"
 	elif [ "$2" = -cottageroar ] || [ "$2" = -mcot ] || [ "$2" = -mcott ] || [ "$2" = -mcr ] || [ "$2" = -cr ]; then
-		echo "cottageroar"
+		monster="cottageroar"
 	elif [ "$2" = -frostwing ] || [ "$2" = -mfw ] || [ "$2" = -mfro ] || [ "$2" = -fro ] || [ "$2" = -frost ] || [ "$2" = -fw ]; then
-		echo "frostwing"
+		monster="frostwing"
 	elif [ "$2" = -gargantua ] || [ "$2" = -mgar ] || [ "$2" = -mgarg ] || [ "$2" = -garg ] || [ "$2" = -gar ]; then
-		echo "gargantua"
+		monster="gargantua"
 	elif [ "$2" = -gawrilla ] || [ "$2" = -tusker ] || [ "$2" = -mtus ] || [ "$2" = -mtusk ] || [ "$2" = -tusk ] || [ "$2" = -mgaw ] || [ "$2" = -mgawr ]; then
-		echo "tusker"
+		monster="tusker"
 	elif [ "$2" = -grim ] || [ "$2" = -reaper ] || [ "$2" = -mgri ] || [ "$2" = -mgr ] || [ "$2" = -gr ]; then
-		echo "grim reaper"
+		monster="grim reaper"
 	elif [ "$2" = -gryphon ] || [ "$2" = -mgry ] || [ "$2" = -grif ]; then
-		echo "gryphon"
+		monster="gryphon"
 	elif [ "$2" = -hardrox ] || [ "$2" = -mhar ] || [ "$2" = -mhr ] || [ "$2" = -rox ] || [ "$2" = -hard ] || [ "$2" = -hr ]; then
-		echo "hardrox"
+		monster="hardrox"
 	elif [ "$2" = -hell ] || [ "$2" = -drider ] || [ "$2" = -mhel ] || [ "$2" = -mhd ] || [ "$2" = -hd ]; then
-		echo "hell drider"
+		monster="hell drider"
 	elif [ "$2" = -hootclaw ] || [ "$2" = -mhoo ] || [ "$2" = -mhc ] || [ "$2" = -hc ] || [ "$2" = -hoot ]; then
-		echo "hootclaw"
+		monster="hootclaw"
 	elif [ "$2" = -jade ] || [ "$2" = -wyrm ] || [ "$2" = -mjad ] || [ "$2" = -mjw ] || [ "$2" = -worm ]; then
-		echo "jade wyrm"
+		monster="jade wyrm"
 	elif [ "$2" = -mecha ] || [ "$2" = -trojan ] || [ "$2" = -mmec ] || [ "$2" = -mmt ] || [ "$2" = -mt ]; then
-		echo "mecha trojan"
+		monster="mecha trojan"
 	elif [ "$2" = -mega ] || [ "$2" = -maggot ] || [ "$2" = -mmeg ] || [ "$2" = -mmm ] || [ "$2" = -magg ] || [ "$2" = -mm ]; then
-		echo "mega maggot"
+		monster="mega maggot"
 	elif [ "$2" = -necrosis ] || [ "$2" = -mnec ] || [ "$2" = -dead ] || [ "$2" = -zombie ] || [ "$2" = -mzom ] || [ "$2" = -mzomb ] || [ "$2" = -neck ]; then
-		echo "necrosis"
+		monster="necrosis"
 	elif [ "$2" = -noceros ] || [ "$2" = -mnor ] || [ "$2" = -noce ] || [ "$2" = -rhino ]; then
-		echo "noceros"
+		monster="noceros"
 	elif [ "$2" = -queen ] || [ "$2" = -bee ] || [ "$2" = -mque ] || [ "$2" = -mqb ] || [ "$2" = -qb ]; then
-		echo "queen bee"
+		monster="queen bee"
 	elif [ "$2" = -saberfang ] || [ "$2" = -msab ] || [ "$2" = -msf ] || [ "$2" = -saber ] || [ "$2" = -fang ] || [ "$2" = -sf ]; then
-		echo "saberfang"
+		monster="saberfang"
 	elif [ "$2" = -serpent ] || [ "$2" = -gladiator ] || [ "$2" = -mser ] || [ "$2" = -serp ] || [ "$2" = -msg ] || [ "$2" = -glad ] || [ "$2" = -sg ]; then
-		echo "serpent gladiator"
+		monster="serpent gladiator"
 	elif [ "$2" = -snow ] || [ "$2" = -beast ] || [ "$2" = -msno ] || [ "$2" = -msb ] || [ "$2" = -sb ]; then
-		echo "snow beast"
+		monster="snow beast"
 	elif [ "$2" = -terrorthorn ] || [ "$2" = -mter ] || [ "$2" = -terror ] || [ "$2" = -thorn ] || [ "$2" = -mthorn ]; then
-		echo "terrorthorn"
+		monster="terrorthorn"
 	elif [ "$2" = -tidal ] || [ "$2" = -titan ] || [ "$2" = -mtid ] || [ "$2" = -mtt ] || [ "$2" = -tt ]; then
-		echo "tidal titan"
+		monster="tidal titan"
 	elif [ "$2" = -voodoo ] || [ "$2" = -shaman ] || [ "$2" = -mvoo ] || [ "$2" = -mvs ] || [ "$2" = -vs ]; then
-		echo "voodoo shaman"
+		monster="voodoo shaman"
 	else
 		echo "monster error"
 	fi
